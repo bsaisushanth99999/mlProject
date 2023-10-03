410a864592cc2ccca13361b7accf17eef68efa5c
BynagariSaiSushanthReddy <bsaisushanth@gmail.com>
2023-10-03 08:05:16 +0530

    setip.py and requirments.txt added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirments(file_path:str)->List[str]:
+    ''' This function will return a list of requirments'''
+    requirments=[]
+    with open(file_path) as filr_obj: 
+        requirments = filr_obj.readlines()
+        requirments = [req.replace("\n", "") for req in requirments]
+
+        if HYPEN_E_DOT in requirments :
+            requirments.remove(HYPEN_E_DOT)
+
+    return requirments
+
+setup(
+    name='mlproject',version='0.0.1', author='kunal', author_email='bsaisushanth@gmail.com',packages=find_packages(), install_requires=get_requirments('requirments.txt')
+)
